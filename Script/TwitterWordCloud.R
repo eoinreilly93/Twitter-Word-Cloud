25e695097d784c83ee48011e10eddb8018165721
Eoin Reilly <Eoin Reilly>
2017-03-18 15:51:58 +0000

    Code formatting

--- Script/TwitterWordCloud.R
+++ Script/TwitterWordCloud.R
@@ -1,5 +1,5 @@
 #############################
-# SET UP TWITTER STUFF
+# SET UP TWITTER CREDENTIALS
 #############################
 
 #NEED DEVTOOLS TO INSTALL GITHUB
@@ -14,33 +14,41 @@ setup_twitter_oauth("vPWn7f3xgbZ1PxMikYnDDFgxl", "tQgOyOiVT6fIAQ2M5WCqYLZzC2TK0D
 # ANALYSIS
 ########################
 
+#GATHER TWEETS FROM TWITTER API FOR SELECTED USER
 mht=userTimeline('realDonaldTrump',n=3200, includeRts=TRUE)
 mht = strip_retweets(mht, strip_manual = TRUE, strip_mt = TRUE)
+
+#CREATE DATAFRAME FROM TWEETS
 tweetsdf <- twListToDF(mht)
 
+#FILTER BY DATE IF NECESSARY
+tweetsdf$created <- format(as.Date(tweetsdf$created,format="%Y-%m-%d"))
+tweetsdf <- subset(tweetsdf, created >= as.Date("2016-11-09"))
 
+#SAVE TO FILE
+write.csv(tweetsdf, "Resources/Datasets/DonaldTrumpTweets18_03_17.csv")
+
+#EXTRACT AND CLEAN UP TEXT FROM TWEETS
 mach_text = sapply(mht, function(x) x$getText())
-
-
 clean_text = clean.text(mach_text)
 tweet_corpus = Corpus(VectorSource(clean_text))
+tdm = TermDocumentMatrix(tweet_corpus,control = list(removePunctuation = TRUE,stopwords = c("the", "and", stopwords("english")),removeNumbers = TRUE, tolower = TRUE))
 
-
-tdm = TermDocumentMatrix(tweet_corpus,control = list(removePunctuation = TRUE,stopwords = c("machine", "learning", "the", "and", stopwords("english")),removeNumbers = TRUE, tolower = TRUE))
-
-# define tdm as matrix
+#DEFINE TDM AS MATRIX
 m = as.matrix(tdm)
-# get word counts in decreasing order
+
+#gET WORD COUNTS IN DECRESING ORDER
 word_freqs = sort(rowSums(m), decreasing=TRUE) 
-# create a data frame with words and their frequencies
+
+#CREATE DATAFRAME OF EACH WORD AND ITS FREQUENCY
 dm = data.frame(word=names(word_freqs), freq=word_freqs)
 
+#PLOT THE WORDCLOUD
 library(wordcloud)
-# plot wordcloud
 wordcloud(dm$word, dm$freq, random.order=FALSE, colors=brewer.pal(8, "Dark2"), max.words = 150)
 
 
-
+#FUNCTION TO CLEAN TEXT IN TWEETS
 clean.text <- function(some_txt)
 {
   some_txt = gsub("&amp", "", some_txt)
@@ -62,7 +70,6 @@ clean.text <- function(some_txt)
   # define "tolower error handling" function
   
   try.tolower = function(x)
-    
   {
     
     y = NA
@@ -74,22 +81,11 @@ clean.text <- function(some_txt)
       y = tolower(x)
     
     return(y)
-    
   }
   
   some_txt = sapply(some_txt, try.tolower)
-  
   some_txt = some_txt[some_txt != ""]
-  
   names(some_txt) = NULL
   
   return(some_txt)
-  
 }
-
-
-#FILTER BY DATE
-tweetsdf$created <- format(as.Date(tweetsdf$created,format="%Y-%m-%d"))
-tweetsdf <- subset(tweetsdf, created >= as.Date("2016-11-09"))
-
-
