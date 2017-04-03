b9ca6565da470321a2fc7bc744493080ba785ff9
Eoin <eoindreilly@gmail.com>
2017-04-03 22:44:20 +0100

    Adding code to include in clean_text file in this script

--- Script/TwitterWordCloud.R
+++ Script/TwitterWordCloud.R
@@ -1,3 +1,6 @@
+#INCLUDE NECESSARY FILES
+source("Script/CleanTweetText.R")
+
 #############################
 # SET UP TWITTER CREDENTIALS
 #############################
@@ -46,46 +49,3 @@ dm = data.frame(word=names(word_freqs), freq=word_freqs)
 #PLOT THE WORDCLOUD
 library(wordcloud)
 wordcloud(dm$word, dm$freq, random.order=FALSE, colors=brewer.pal(8, "Dark2"), max.words = 150)
-
-
-#FUNCTION TO CLEAN TEXT IN TWEETS
-clean.text <- function(some_txt)
-{
-  some_txt = gsub("&amp", "", some_txt)
-  
-  some_txt = gsub("(RT|via)((?:\b\\W*@\\w+)+)", "", some_txt)
-  
-  some_txt = gsub("@\\w+", "", some_txt)
-  
-  some_txt = gsub("[[:punct:]]", "", some_txt)
-  
-  some_txt = gsub("[[:digit:]]", "", some_txt)
-  
-  some_txt = gsub("http\\w+", "", some_txt)
-  
-  some_txt = gsub("[t]{2,}", "", some_txt)
-  
-  some_txt = gsub("^\\s+|\\s+$", "", some_txt)
-  
-  # define "tolower error handling" function
-  
-  try.tolower = function(x)
-  {
-    
-    y = NA
-    
-    try_error = tryCatch(tolower(x), error=function(e) e)
-    
-    if (!inherits(try_error, "error"))
-      
-      y = tolower(x)
-    
-    return(y)
-  }
-  
-  some_txt = sapply(some_txt, try.tolower)
-  some_txt = some_txt[some_txt != ""]
-  names(some_txt) = NULL
-  
-  return(some_txt)
-}
